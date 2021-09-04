55b7c5396c31692bf63821d898801db08c82caa5
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-09-04 16:25:29 -0300

    Create Onboarding

---
+++ src/utils/slides.js
@@ -0,0 +1,26 @@
+export const slides = [
+  {
+    id: '1',
+    title: 'Page 1',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    image: require('../assets/images/image1.png'),
+  },
+  {
+    id: '2',
+    title: 'Page 2',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry2.',
+    image: require('../assets/images/image2.png'),
+  },
+  {
+    id: '3',
+    title: 'Page 3',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry3.',
+    image: require('../assets/images/image3.png'),
+  },
+  {
+    id: '4',
+    title: 'Page 4',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry4.',
+    image: require('../assets/images/image4.png'),
+  },
+]
